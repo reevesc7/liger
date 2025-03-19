4526a4cca169cf3ae360cc08881c2b5c44f13876
Reeves <reeves.chris.allan@gmail.com>
2025-03-19 15:21:15 -0400

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="liger",
-    version="0.2.0",
+    version="0.2.1",
     entry_points={ "console_scripts": [
         "liger-pipeline=liger.run_tpot_pipeline:main",
     ]},
