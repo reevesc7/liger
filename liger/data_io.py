fc1b372c7a98188dea5ac6b350bf2cb72b9c93c1
Chris Reeves <reeves.chris.allan@gmail.com>
2025-03-21 02:11:06 -0400

    Add licnse header to source files

--- liger/data_io.py
+++ liger/data_io.py
@@ -1,3 +1,20 @@
+# liger - Helper functions for the Likert General Regressor project
+# Copyright (C) 2024  Chris Reeves
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import numpy as np
 from numpy.typing import ArrayLike
 import random
@@ -78,3 +95,4 @@ def plot_expl_var_ratios(data: ArrayLike, title: str = None, max_components: int
         plt.show()
     plt.close()
     return expl_var_ratios
+
--- liger/operations.py
+++ liger/operations.py
@@ -1,3 +1,20 @@
+# liger - Helper functions for the Likert General Regressor project
+# Copyright (C) 2024  Chris Reeves
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import numpy as np
 from numpy.typing import ArrayLike
 
@@ -25,3 +42,4 @@ def vector_rejection(ABvector: ArrayLike, APvector: ArrayLike) -> float:
 def compute_R2(data: list[ArrayLike], equations: list) -> list:
     actual_y = data[1]
     return [np.corrcoef(actual_y, [equation(x) for x in data[0]])[0,1]**2 for equation in equations]
+
