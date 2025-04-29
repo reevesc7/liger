c9ef88e705c04c16d2475c6264095d1c1df30ba3
Chris Reeves <reeves.chris.allan@gmail.com>
2025-04-29 19:13:00 -0400

    Remove commented code

--- liger/search_space_creator.py
+++ liger/search_space_creator.py
@@ -47,33 +47,24 @@ def items_to_search_space(node_type: str, node_parameters: Any, n_features: int,
     match node_type:
         case "ChoicePipeline":
             search_space = tpot.search_spaces.pipelines.ChoicePipeline(**node_kwargs)
-            #search_space = construct_choice_pipeline(node_parameters, n_features, random_state)
         case "SequentialPipeline":
             search_space = tpot.search_spaces.pipelines.SequentialPipeline(**node_kwargs)
-            #search_space = construct_sequential_pipeline(node_parameters, n_features, random_state)
         case "DynamicLinearPipeline":
             search_space = tpot.search_spaces.pipelines.DynamicLinearPipeline(**node_kwargs)
-            #search_space = construct_dynamic_linear_pipeline(node_parameters, n_features, random_state)
         case "UnionPipeline":
             search_space = tpot.search_spaces.pipelines.UnionPipeline(**node_kwargs)
-            #search_space = construct_union_pipeline(node_parameters, n_features, random_state)
         case "DynamicUnionPipeline":
             search_space = tpot.search_spaces.pipelines.DynamicUnionPipeline(**node_kwargs)
-            #search_space = construct_dynamic_union_pipeline(node_parameters, n_features, random_state)
         case "TreePipeline":
             search_space = tpot.search_spaces.pipelines.TreePipeline(**node_kwargs)
-            #search_space = construct_tree_pipeline(node_parameters, n_features, random_state)
         case "GraphSearchPipeline":
             search_space = tpot.search_spaces.pipelines.GraphSearchPipeline(**node_kwargs)
-            #search_space = construct_graph_search_pipeline(node_parameters, n_features, random_state)
         case "EstimatorNode":
             search_space = tpot.config.get_search_space(node_parameters["class_name"], random_state=random_state)
             if not isinstance(search_space, (tpot.search_spaces.nodes.EstimatorNode, tpot.search_spaces.pipelines.ChoicePipeline)):
                 raise ValueError(f"{node_parameters['class_name']} could not be converted into an EstimatorNode or ChoicePipeline")
-            #search_space = construct_estimator_node(node_parameters, random_state)
         case "GeneticFeatureSelectorNode":
             search_space = tpot.search_spaces.nodes.GeneticFeatureSelectorNode(n_features, **node_kwargs)
-            #search_space = construct_genetic_feature_selector_node(node_parameters, n_features)
         case _:
             raise ValueError(f"{node_type} does not match a TPOT pipeline or node type (WrapperPipeline not included)")
     return search_space
