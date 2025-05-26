cd8d5ad6edb25b2b883ac8b02a9fd24a6fb450d7
Chris Reeves <reeves.chris.allan@gmail.com>
2025-05-26 13:31:24 -0400

    Edits made to output field retrieval during testing

--- liger/output_processing.py
+++ liger/output_processing.py
@@ -1,10 +1,10 @@
-from typing import Any, overload
+from typing import Any, Sequence, overload
 from pathlib import Path
 import os
 import json
 
 
-def _dirpaths_and_filepaths_to_filepaths(paths: list[Path]) -> set[Path]:
+def _dirpaths_and_filepaths_to_filepaths(paths: Sequence[Path]) -> set[Path]:
     filepaths: set[Path] = set()
     for path in paths:
         if not path.exists():
@@ -15,18 +15,26 @@ def _dirpaths_and_filepaths_to_filepaths(paths: list[Path]) -> set[Path]:
             print(f"Warning: {path} is exists but is not a directory or file")
             continue
         for base, _, names in os.walk(path):
-            filepaths.update(set(Path(base, name) for name in names))
+            filepaths.update({Path(base, name) for name in names})
     return filepaths
 
 
 def mass_json_load(
-    paths: Path | list[Path],
+    paths: Path | str | Sequence[Path | str],
     read_all_json_files: bool = False,
-    filenames_to_read: set[str] = set("pipeline_data.json"),
+    filenames_to_read: str | set[str] = "pipeline_data.json",
 ) -> list[Any]:
     if isinstance(paths, Path):
-        paths = [paths]
-    filepaths = _dirpaths_and_filepaths_to_filepaths(paths)
+        paths_f = [paths]
+    elif isinstance(paths, str):
+        paths_f = [Path(paths)]
+    elif isinstance(paths, Sequence):
+        paths_f = [Path(path) for path in paths]
+    else:
+        raise TypeError("`paths` must be str, Path, or sequence of str or Path")
+    if isinstance(filenames_to_read, str):
+        filenames_to_read = {filenames_to_read,}
+    filepaths = _dirpaths_and_filepaths_to_filepaths(paths_f)
     outputs: list[Any] = []
     for filepath in filepaths:
         if (
@@ -42,11 +50,11 @@ def mass_json_load(
 _sentinel = object()
 
 @overload
-def mass_dict_get(dicts: list[dict[Any, Any]], key: Any) -> list[Any]: ...
+def mass_dict_get(dicts: Sequence[dict[str, Any]], key: str) -> list[Any]: ...
 @overload
-def mass_dict_get(dicts: list[dict[Any, Any]], key: Any, default: Any) -> list[Any]: ...
+def mass_dict_get(dicts: Sequence[dict[str, Any]], key: str, default: Any) -> list[Any]: ...
 
-def mass_dict_get(dicts: list[dict[Any, Any]], key: Any, default: Any = _sentinel) -> list[Any]:
+def mass_dict_get(dicts: Sequence[dict[str, Any]], key: str, default: Any = _sentinel) -> list[Any]:
     if default is _sentinel:
         return [dictionary.get(key) for dictionary in dicts]
     return [dictionary.get(key, default) for dictionary in dicts]
