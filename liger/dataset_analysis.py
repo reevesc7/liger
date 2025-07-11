5659d6f8c6da4e6e2a7c13435795c3f4c44f3c83
Chris Reeves <reeves.chris.allan@gmail.com>
2025-07-11 14:11:11 -0400

    Remove `operations.py`, add `dataset_analysis.linear_fit()` (untested)

--- liger/dataset_analysis.py
+++ liger/dataset_analysis.py
@@ -21,6 +21,34 @@ import pandas as pd
 from sklearn.decomposition import PCA
 
 
+def _scalar_projection(vector: ArrayLike, target: ArrayLike) -> float:
+    return np.dot(vector, target) / np.linalg.norm(target)
+
+
+def _rejection(vector: ArrayLike, target: ArrayLike) -> float:
+    target = np.asarray(target)
+    vector = np.asarray(vector)
+    projection = _scalar_projection(target, vector) * target
+    return float(np.linalg.norm(vector - projection))
+
+
+def linear_fit(data: pd.DataFrame, point1: ArrayLike, point2: ArrayLike) -> pd.DataFrame:
+    """Squash the data onto a line segment between two points and report the proportional
+    distance of each point along the line segment (alpha) and the distance of each
+    point from the line segment (deviation).
+
+    ...
+    """
+    point1 = np.asarray(point1)
+    point2 = np.asarray(point2)
+    target = point2 - point1
+    x = data.x.apply(lambda row: (_scalar_projection(row, target), _rejection(row, target)))
+    if not isinstance(x, pd.DataFrame):
+        raise TypeError("This error shouldn't have happened. x is not a DataFrame")
+    x.columns = pd.Index("alpha", "deviation")
+    return x
+
+
 def pca(data: pd.DataFrame, n_components: int | None = None) -> tuple[PCA, pd.DataFrame]:
     pca = PCA(n_components)
     reduced_data = pca.fit_transform(data)
