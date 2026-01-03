c46367ffccad6ba35272a589d76922b5e20e26e8
Chris <reeves.chris.allan@gmail.com>
2026-01-03 14:41:26 -0500

    Add `LgTransformedTargetRegressor` parsing to `search_space_creator`

--- liger/tpot/search_space_creator.py
+++ liger/tpot/search_space_creator.py
@@ -16,14 +16,18 @@
 
 
 from typing import Any
-import tpot
+from tpot import search_spaces as tpss
+from tpot import config as tpcfg
+from ConfigSpace import ConfigurationSpace
+from ConfigSpace.hyperparameters.hyperparameter import Hyperparameter
+from liger import sklearn as lsk
 
 
 def create_search_space(
     param_search_space: Any,
     n_features: int,
     random_state: int | None = None
-) -> tpot.search_spaces.SearchSpace:
+) -> tpss.SearchSpace:
     search_space = items_to_search_space(
         param_search_space["node_type"],
         {key: value for key, value in param_search_space.items() if key != "node_type"},
@@ -37,11 +41,41 @@ def create_search_spaces(
     param_search_spaces: Any,
     n_features: int,
     random_state: int | None = None
-) -> list[tpot.search_spaces.SearchSpace]:
+) -> list[tpss.SearchSpace]:
     search_spaces = []
     for param_search_space in param_search_spaces:
         search_spaces.append(create_search_space(param_search_space, n_features, random_state))
     return search_spaces
+
+
+def _append_to_hyperparameter_name(hyperparameter: Hyperparameter, add: str) -> Hyperparameter:
+    hyperparameter.name = add + hyperparameter.name
+    return hyperparameter
+
+
+def _make_lg_transformed_target_regressor(
+    node_parameters: dict[str, str],
+    random_state: int | None = None,
+) -> tpss.SearchSpace:
+    rg_node = tpcfg.get_search_space(node_parameters["regressor"], random_state=random_state)
+    tf_node = tpcfg.get_search_space(node_parameters["transformer"], random_state=random_state)
+    if not isinstance(rg_node, tpss.nodes.EstimatorNode):
+        raise ValueError(f"{node_parameters["regressor"]} could not be converted into an EstimatorNode")
+    if not isinstance(tf_node, tpss.nodes.EstimatorNode):
+        raise ValueError(f"{node_parameters["transformer"]} could not be converted into an EstimatorNode")
+    models = {"regressor": rg_node.method, "transformer": tf_node.method}
+    rg_params = {
+        "regressor_" + key: _append_to_hyperparameter_name(value, "regressor_")
+        for key, value in rg_node.space.items()
+    }
+    tf_params = {
+        "transformer_" + key: _append_to_hyperparameter_name(value, "transformer_")
+        for key, value in tf_node.space.items()
+    }
+    return tpss.nodes.EstimatorNode(
+        method = lsk.LgTransformedTargetRegressor,
+        space = ConfigurationSpace(space=models | rg_params | tf_params),
+    )
 
 
 def items_to_search_space(
@@ -49,7 +83,7 @@ def items_to_search_space(
     node_parameters: Any,
     n_features: int,
     random_state: int | None = None
-) -> tpot.search_spaces.SearchSpace:
+) -> tpss.SearchSpace:
     node_kwargs = {}
     for key, value in node_parameters.items():
         if key == "search_spaces":
@@ -59,28 +93,30 @@ def items_to_search_space(
     node_kwargs.update({key: value for key, value in node_parameters.items() if "search_space" not in key})
     match node_type:
         case "ChoicePipeline":
-            search_space = tpot.search_spaces.pipelines.ChoicePipeline(**node_kwargs)
+            search_space = tpss.pipelines.ChoicePipeline(**node_kwargs)
         case "SequentialPipeline":
-            search_space = tpot.search_spaces.pipelines.SequentialPipeline(**node_kwargs)
+            search_space = tpss.pipelines.SequentialPipeline(**node_kwargs)
         case "DynamicLinearPipeline":
-            search_space = tpot.search_spaces.pipelines.DynamicLinearPipeline(**node_kwargs)
+            search_space = tpss.pipelines.DynamicLinearPipeline(**node_kwargs)
         case "UnionPipeline":
-            search_space = tpot.search_spaces.pipelines.UnionPipeline(**node_kwargs)
+            search_space = tpss.pipelines.UnionPipeline(**node_kwargs)
         case "DynamicUnionPipeline":
-            search_space = tpot.search_spaces.pipelines.DynamicUnionPipeline(**node_kwargs)
+            search_space = tpss.pipelines.DynamicUnionPipeline(**node_kwargs)
         case "TreePipeline":
-            search_space = tpot.search_spaces.pipelines.TreePipeline(**node_kwargs)
+            search_space = tpss.pipelines.TreePipeline(**node_kwargs)
         case "GraphSearchPipeline":
-            search_space = tpot.search_spaces.pipelines.GraphSearchPipeline(**node_kwargs)
+            search_space = tpss.pipelines.GraphSearchPipeline(**node_kwargs)
         case "EstimatorNode":
-            search_space = tpot.config.get_search_space(node_parameters["class_name"], random_state=random_state)
+            search_space = tpcfg.get_search_space(node_parameters["class_name"], random_state=random_state)
             if not isinstance(search_space, (
-                tpot.search_spaces.nodes.EstimatorNode,
-                tpot.search_spaces.pipelines.ChoicePipeline,
+                tpss.nodes.EstimatorNode,
+                tpss.pipelines.ChoicePipeline,
             )):
-                raise ValueError(f"{node_parameters['class_name']} could not be converted into an EstimatorNode or ChoicePipeline")
+                raise ValueError(f"{node_parameters["class_name"]} could not be converted into an EstimatorNode or ChoicePipeline")
         case "GeneticFeatureSelectorNode":
-            search_space = tpot.search_spaces.nodes.GeneticFeatureSelectorNode(n_features, **node_kwargs)
+            search_space = tpss.nodes.GeneticFeatureSelectorNode(n_features, **node_kwargs)
+        case "LgTransformedTargetRegressor":
+            search_space = _make_lg_transformed_target_regressor(node_kwargs, random_state)
         case _:
             raise ValueError(f"{node_type} does not match a TPOT pipeline or node type (WrapperPipeline not included)")
     return search_space
