c44336df9eb08f2b1b83118da57b9541c634c598
Chris <reeves.chris.allan@gmail.com>
2026-01-08 02:34:02 -0500

    Add `score_wasserstein()`, improve function signatures in `objectives`

--- liger/objectives.py
+++ liger/objectives.py
@@ -15,30 +15,40 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from typing import Any
+from typing import Sequence
 from sklearn.metrics import make_scorer
 import numpy as np
-from numpy.typing import NDArray
+from numpy.typing import ArrayLike
+from scipy.stats import wasserstein_distance
+from liger import probabilities as prb
 
 
-def score_msle_2d(y_true: NDArray, y_pred: NDArray) -> np.floating[Any]:
+def score_msle_2d(y_true: ArrayLike, y_pred: ArrayLike) -> np.floating:
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
     d1_true, d2_true = y_true[:, 0], y_true[:, 1]
     d1_pred, d2_pred = y_pred[:, 0], y_pred[:, 1]
     return score_msle(d1_true, d1_pred) + score_msle(d2_true, d2_pred)
 
 
-def score_msle(y_true: NDArray, y_pred: NDArray) -> np.floating[Any]:
+def score_msle(y_true: ArrayLike, y_pred: ArrayLike) -> np.floating:
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
     y_true_clip = np.clip(y_true, 1e-6, None)
     y_pred_clip = np.clip(y_pred, 1e-6, None)
     return np.mean(np.log(y_pred_clip) - np.log(y_true_clip))**2
 
 
-def score_nll_mean_stdev(y_true: NDArray, y_pred: NDArray) -> np.floating[Any]:
+def score_nll_mean_stdev(y_true: ArrayLike, y_pred: ArrayLike) -> np.floating:
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
     mean_pred, stdev_pred = y_pred[:, 0], y_pred[:, 1]
     return np.mean(np.log(stdev_pred) + (y_true - mean_pred)**2 / (2 * stdev_pred**2))
 
 
-def score_test(y_true: NDArray, y_pred: NDArray) -> np.floating[Any]:
+def score_test(y_true: ArrayLike, y_pred: ArrayLike) -> np.floating:
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
     mean_true = y_true[:, 0]
     mean_pred, stdev_pred = y_pred[:, 0], y_pred[:, 1]
     a = (mean_pred - mean_true)**2
@@ -49,8 +59,50 @@ def score_test(y_true: NDArray, y_pred: NDArray) -> np.floating[Any]:
     return np.mean(a + b + c + d - e)
 
 
-# def soft_brier(y_true_pmf: NDArray, y_pred_proba: NDArray) -> np.floating[Any]:
+# def soft_brier(y_true_pmf: ArrayLike, y_pred_proba: ArrayLike) -> np.floating[Any]:
+    # y_true = np.asarray(y_true)
+    # y_pred = np.asarray(y_pred)
 #     return np.mean(np.sum((y_pred_proba - y_true_pmf) ** 2, axis=1))
+
+
+def _row_wasserstein(
+    pmf_true: ArrayLike,
+    pmf_pred: ArrayLike,
+    x_vals: Sequence[float],
+) -> float:
+    pmf_true = np.asarray(pmf_true)
+    pmf_pred = np.asarray(pmf_pred)
+    if pmf_true.ndim != 1:
+        raise ValueError(f"Expected 1D array-like inputs, but pmf_true is shape {pmf_true.shape}")
+    if pmf_pred.ndim != 1:
+        raise ValueError(f"Expected 1D array-like inputs, but pmf_pred is shape {pmf_pred.shape}")
+    return wasserstein_distance(x_vals, x_vals, pmf_true, pmf_pred)
+
+
+def score_wasserstein(
+    y_true: ArrayLike,
+    y_pred: ArrayLike,
+    x_vals: Sequence,
+) -> np.floating:
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+    distances = np.full(y_true.shape[0], 0.0)
+    for i in range(len(distances)):
+        distances[i] = _row_wasserstein(y_true[i], y_pred[i], x_vals)
+    return np.mean(distances)
+
+
+def score_softmax_wasserstein(
+    y_true: ArrayLike,
+    y_pred: ArrayLike,
+    x_vals: Sequence,
+    temperature: float = 1.0,
+) -> np.floating:
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+    pmf_true = prb.softmax(y_true, temperature)
+    pmf_pred = prb.softmax(y_pred, temperature)
+    return score_wasserstein(pmf_true, pmf_pred, x_vals)
 
 
 # Define scorers
@@ -69,4 +121,21 @@ neg_test = make_scorer(
 #     response_method="predict_proba",
 #     greater_is_better=False,
 # )
+neg_wasserstein = make_scorer(
+    score_wasserstein,
+    response_method="predict",
+    greater_is_better=False,
+    **{
+        "x_vals": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+)
+neg_softmax_wasserstein = make_scorer(
+    score_softmax_wasserstein,
+    response_method="predict",
+    greater_is_better=False,
+    **{
+        "x_vals": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        "temperature": 1.0,
+    },
+)
 
