187a57793096a81a7e59aae89f8d6bab9d22c9ad
Chris <reeves.chris.allan@gmail.com>
2026-03-07 14:20:43 -0500

    Switch to git-tag-based version control

--- src/liger/__init__.py
+++ src/liger/__init__.py
@@ -14,3 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
+from importlib.metadata import version
+
+
+__version__ = version("liger")
+
