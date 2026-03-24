30cdfd67d02de6f6dd8eea039da077efb94c97cb
Chris <reeves.chris.allan@gmail.com>
2026-03-24 05:03:32 -0400

    Add workaround for broken `stopit`, dependency of `tpot`

--- src/liger/tpot/manager.py
+++ src/liger/tpot/manager.py
@@ -15,6 +15,22 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+# pkg_resources.get_distribution() replacement for stopit compatibility
+import importlib.util
+
+if importlib.util.find_spec("pkg_resources") is None:
+    from types import ModuleType
+    from importlib.metadata import version
+    import sys
+    fake_pkg_resources = ModuleType("pkg_resources")
+    setattr(
+        fake_pkg_resources,
+        "get_distribution",
+        lambda name: type("Distribution", (), {"version": version(name)})(),
+    )
+    sys.modules["pkg_resources"] = fake_pkg_resources
+
+
 from typing import Any, Callable, Self, TextIO
 import sys
 from pathlib import Path
