0d2e6b54ce8e907add10c3c3a3ec2de8092e534f
Chris <reeves.chris.allan@gmail.com>
2026-06-07 06:45:03 -0400

    Remove trailing whitespaces in license header

--- src/liger/__init__.py
+++ src/liger/__init__.py
@@ -1,11 +1,11 @@
 # liger - Helper functions for the Likert General Regressor project
 # Copyright (C) 2024  Chris Reeves
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
